04d51e66645015f4116881497420ecceb746f919
“camilo <andresml2796@gmail.com>
2023-03-08 17:09:54 -0300

    mi first commit

---
+++ Ejercicio1.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ejercicio1;
+
+/**
+ *
+ * @author ANDRES
+ */
+public class Ejercicio1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        int num1 = 12;
+        int num2 = 8;
+        
+        int suma = num1 + num2;
+        System.out.println("suma" + suma);
+        
+    } 
+    
+}
+
